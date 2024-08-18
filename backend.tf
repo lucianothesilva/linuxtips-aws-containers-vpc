8dee03400bd3039a2c19c1a341a917347f4c3af1
Luciano <lucianodefranca@outlook.com>
2024-08-18 17:43:46 -0300

    feat: add parameters store

--- backend.tf
+++ backend.tf
@@ -2,5 +2,4 @@ terraform {
   backend "s3" {
 
   }
-
 }
--- private_subnets.tf
+++ private_subnets.tf
@@ -75,7 +75,7 @@ resource "aws_route_table_association" "private_1b" {
    route_table_id = aws_route_table.private_internet_access_1b.id
 }
 
-resource "aws_route_table_association" "private_1a" {
+resource "aws_route_table_association" "private_1c" {
   subnet_id = aws_subnet.private_subnet_1c.id
    route_table_id = aws_route_table.private_internet_access_1c.id
 }

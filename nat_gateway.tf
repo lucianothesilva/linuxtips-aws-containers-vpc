f5b1b13b04e93d2379d9d98d91793932e2faffa7
Luciano <lucianodefranca@outlook.com>
2024-09-02 20:17:19 -0300

    fix: outputs an subnets

--- nat_gateway.tf
+++ nat_gateway.tf
@@ -7,8 +7,8 @@ resource "aws_eip" "vpc_eip_1a" {
 }
 
 resource "aws_nat_gateway" "nat_1a" {
-  allocation_id = aws_eip.vpc_eip_1a.vpc_id
-  subnet_id     = aws_subnet.public_subnet_1a
+  allocation_id = aws_eip.vpc_eip_1a.id
+  subnet_id     = aws_subnet.public_subnet_1a.id
   tags = {
     Name = format("%s-nat-1a", var.project_name)
   }
@@ -24,8 +24,8 @@ resource "aws_eip" "vpc_eip_1b" {
 }
 
 resource "aws_nat_gateway" "nat_1b" {
-  allocation_id = aws_eip.vpc_eip_1b.vpc_id
-  subnet_id     = aws_subnet.public_subnet_1b
+  allocation_id = aws_eip.vpc_eip_1b.id
+  subnet_id     = aws_subnet.public_subnet_1b.id
   tags = {
     Name = format("%s-nat-1b", var.project_name)
   }
@@ -41,8 +41,8 @@ resource "aws_eip" "vpc_eip_1c" {
 }
 
 resource "aws_nat_gateway" "nat_1c" {
-  allocation_id = aws_eip.vpc_eip_1c.vpc_id
-  subnet_id     = aws_subnet.public_subnet_1c
+  allocation_id = aws_eip.vpc_eip_1c.id
+  subnet_id     = aws_subnet.public_subnet_1c.id
   tags = {
     Name = format("%s-nat-1c", var.project_name)
   }
